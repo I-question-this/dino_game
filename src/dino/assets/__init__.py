9bab2a19300acf7f7f1924747756bc4ec2bb3436
Aaron Ruen <ruenan@mail.uc.edu>
2022-02-26 13:28:52 -0500

    added other dino paths

--- src/dino/assets/__init__.py
+++ src/dino/assets/__init__.py
@@ -7,3 +7,6 @@ import os
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DINO_DOUX = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - doux.png")
+DINO_BART = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - bart.png")
+DINO_MORT = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - mort.png")
+DINO_VITA = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - vita.png")
