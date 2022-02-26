53b75f379a4452d079808004d52530a278980175
Tyler Westland <tylerofthewest@gmail.com>
2022-02-26 13:19:43 -0500

    Adds asset importing.

---
+++ src/dino/assets/__init__.py
@@ -0,0 +1,9 @@
+"""Assets directory"""
+
+__author__="Tyler Westland"
+
+import os
+
+ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DINO_DOUX = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - doux.png")
