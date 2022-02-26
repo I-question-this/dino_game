bd8b69b8e72a5a121f55356e6835a0b736a8ebc5
Tyler Westland <tylerofthewest@gmail.com>
2022-02-26 15:34:39 -0500

    Move sprites into assets/sprites directory.

--- src/dino/assets/__init__.py
+++ src/dino/assets/__init__.py
@@ -5,8 +5,9 @@ __author__="Tyler Westland"
 import os
 
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
+SPRITES_DIR = os.path.join(ASSETS_DIR, "sprites")
 
-DINO_DOUX = os.path.join(ASSETS_DIR, "DinoSprites - doux.png")
-DINO_BART = os.path.join(ASSETS_DIR, "DinoSprites - bart.png")
-DINO_MORT = os.path.join(ASSETS_DIR, "DinoSprites - mort.png")
-DINO_VITA = os.path.join(ASSETS_DIR, "DinoSprites - vita.png")
+DINO_DOUX = os.path.join(SPRITES_DIR, "DinoSprites--doux.png")
+DINO_BART = os.path.join(SPRITES_DIR, "DinoSprites--bart.png")
+DINO_MORT = os.path.join(SPRITES_DIR, "DinoSprites--mort.png")
+DINO_VITA = os.path.join(SPRITES_DIR, "DinoSprites--vita.png")
