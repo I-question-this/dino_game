1734521f6a1fd71a25ad34986b6e346ea18a2572
Aaron Ruen <ruenan@mail.uc.edu>
2022-02-26 14:03:59 -0500

    fixed asset import

--- src/dino/assets/__init__.py
+++ src/dino/assets/__init__.py
@@ -6,7 +6,7 @@ import os
 
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DINO_DOUX = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - doux.png")
-DINO_BART = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - bart.png")
-DINO_MORT = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - mort.png")
-DINO_VITA = os.path.join(ASSETS_DIR, "dinoCharacter/sheets/DinoSprites - vita.png")
+DINO_DOUX = os.path.join(ASSETS_DIR, "DinoSprites - doux.png")
+DINO_BART = os.path.join(ASSETS_DIR, "DinoSprites - bart.png")
+DINO_MORT = os.path.join(ASSETS_DIR, "DinoSprites - mort.png")
+DINO_VITA = os.path.join(ASSETS_DIR, "DinoSprites - vita.png")
