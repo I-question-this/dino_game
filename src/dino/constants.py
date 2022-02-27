3a1491fa6fa442a3bf27a82255429f17d38da37b
Tyler Westland <tylerofthewest@gmail.com>
2022-02-26 22:25:14 -0500

    Made view port smaller

--- src/dino/constants.py
+++ src/dino/constants.py
@@ -10,8 +10,8 @@ BLUE = (0, 0, 255)
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-VIEW_WIDTH = 700
-VIEW_HEIGHT = 500
+VIEW_WIDTH = 400
+VIEW_HEIGHT = 300
 VIEW_RECT = pygame.Rect((SCREEN_WIDTH-VIEW_WIDTH)//2,
                             (SCREEN_HEIGHT-VIEW_HEIGHT)//2,
                             VIEW_WIDTH, VIEW_HEIGHT)
